be7c876ff106dc76d0d1deeedfb767fde9dffeeb
GabrielEduardoSilva <102189527+GabrielEduardoSilva@users.noreply.github.com>
2024-06-12 10:30:11 -0300

    Changes

--- lfac/lfac/LANG/Utils/RegexSource.swift
+++ lfac/lfac/LANG/Utils/RegexSource.swift
@@ -13,11 +13,11 @@ struct RegexSource {
     var decimalSign = /^[.|,]{1}$/
     var terminators = /^[\r|\n|\t|\0|\s]$/
     var operators = /^[:|<|>|=|\+|\*|\/|\-]$/
-    var keywords = /[program|var|integer|real|boolean|procedure|begin|end|if|then|else|while|do|or|true|false|div|and|not|READ|WRITE]/
+    var keywords = /^(program|var|integer|real|boolean|procedure|begin|end|if|then|else|while|do|or|true|false|div|and|not|READ|WRITE)$/
     var symbol = /^[.|,|;|:|\(|\)|{|}|\[|\]]/
     var space = /^[\s]+$/
     var commentary = /[\/]{2,}/
-    var booleans = /^[true|false]$/
+    var booleans = /^(true|false)$/
     
     var keywordsArray = ["program", "var", "integer", "real", "boolean", "procedure", "begin", "end", "if", "then", "else", "while", "do", "or", "true", "false", "div", "and", "not", "READ", "WRITE"]
 }
