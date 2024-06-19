eeb9aef31f58e1058d4eb12702aef8f3acc0f332
GabrielEduardoSilva <102189527+GabrielEduardoSilva@users.noreply.github.com>
2024-06-19 10:35:28 -0300

    relação feito

--- lfac/lfac/LANG/SyntacticalAnalyzer.swift
+++ lfac/lfac/LANG/SyntacticalAnalyzer.swift
@@ -295,27 +295,7 @@ final class SyntacticalAnalyzer {
     /// <relação> ::=
     ///     = | <> | < | <= | >= | >
     func relacao() throws {
-        guard tokens[currentTokenIndex].value == "=" else {
-            throw ErrorState.e7
-        }
-        
-        guard tokens[currentTokenIndex].value == "<>" else {
-            throw ErrorState.e7
-        }
-        
-        guard tokens[currentTokenIndex].value == "<" else {
-            throw ErrorState.e7
-        }
-        
-        guard tokens[currentTokenIndex].value == "<=" else {
-            throw ErrorState.e7
-        }
-        
-        guard tokens[currentTokenIndex].value == ">=" else {
-            throw ErrorState.e7
-        }
-        
-        guard tokens[currentTokenIndex].value == ">" else {
+        guard tokens[currentTokenIndex].value == "=" || tokens[currentTokenIndex].value == "<>" || tokens[currentTokenIndex].value == "<" || tokens[currentTokenIndex].value == "<=" || tokens[currentTokenIndex].value == ">=" || tokens[currentTokenIndex].value == ">" else {
             throw ErrorState.e7
         }
     }
